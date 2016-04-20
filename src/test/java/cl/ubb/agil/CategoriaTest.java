be0c66b363da09d1ea361436c51c27ef6c386c96
NicolasOnate <nionate@alumnos.ubiobio.cl>
2016-04-20 12:21:01 -0300

    actualizado test de categoria

--- src/test/java/cl/ubb/agil/CategoriaTest.java
+++ src/test/java/cl/ubb/agil/CategoriaTest.java
@@ -11,7 +11,7 @@ public class CategoriaTest {
 	private Categoria categoria;
 	
 	@Test
-	public void addTwoPlusThreeIsFive(){	
+	public void categoriaGetName(){	
 		/* Arrange */
 		categoria = new Categoria("Guitarras Electricas", "0001");
 		
@@ -22,5 +22,42 @@ public class CategoriaTest {
 		assertThat(resultado,is("Guitarras Electricas"));
 	}
 	
+	@Test
+	public void categoriaGetCode(){	
+		/* Arrange */
+		categoria = new Categoria("Guitarras Electricas", "0001");
+		
+		/* Act */
+		String resultado = categoria.getCodigo();
+		
+		/* Assert */
+		assertThat(resultado,is("0001"));
+	}
+	
+	@Test
+	public void categoriaSetName(){	
+		/* Arrange */
+		categoria = new Categoria("Guitarras Electricas", "0001");
+		
+		/* Act */
+		categoria.setNombre("Guitarras Acústicas");
+		String resultado = categoria.getNombre();
+		
+		/* Assert */
+		assertThat(resultado,is("Guitarras Acústicas"));
+	}
+	
+	@Test
+	public void categoriaSetCode(){	
+		/* Arrange */
+		categoria = new Categoria("Guitarras Electricas", "0001");
+		
+		/* Act */
+		categoria.setCodigo("0002");
+		String resultado = categoria.getCodigo();
+		
+		/* Assert */
+		assertThat(resultado,is("0002"));
+	}
 
 }
